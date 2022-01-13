6c5b7d7a0548e0d0fcaf8c6ac5b291a3de9fb11a
Bruce Rose <brucej.rose@orange.fr>
2022-01-13 18:46:49 +0100

    Finished decide method for QAI

--- player.py
+++ player.py
@@ -26,20 +26,23 @@ class QAI(Player):
     def __init__(self, team):
         super().__init__(team)
         self.eps = QAI.eps_init
+        self.Q = {}
 
     def reset(self):
         self.eps = QAI.eps_init
 
-    def decide(self, env, state, unit): # TODO finish
+    def decide(self, env, state, unit): # DONE finish
         self.eps *= QAI.eps_decay
+        available_actions = env.valid_actions(state, unit) # TODO write that method
+        if state not in self.Q.keys():
+            self.Q[state] = {}
+        for a in available_actions:
+            if a not in self.Q[state].keys():
+                self.Q[state][a] = 0.
         if random.random() < self.eps:
             # action = random action from action space
-            # action = random.choice(np.flatnonzero(observation['action_mask']))
-            # print(agent, "chose a random action")
-            available_actions = env.valid_actions(state, unit) # TODO write that method
-            action = random.choice(available_actions) # TODO comprehensive action space in unit
+            action = unit.actions[random.choice(available_actions)] # TODO comprehensive action space in unit
         else:
             # action = best action from Q = max_i(Q(S, i))
-            # np.flatnonzero(Q[agent][state][action_mask == 1])
-            action = max([i for i in range(nA) if action_mask[i] == 1], key = lambda x: Q[agent][state][x])
+            action = unit.actions[max(available_actions, key = lambda x: self.Q[state][x])]
         return action
